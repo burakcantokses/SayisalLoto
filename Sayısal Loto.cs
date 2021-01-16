0a9cbc24c43e17ac187a137eebb9dd52496448e0
Burakcan TOKSES <75482610+burakcantokses@users.noreply.github.com>
2021-01-16 19:50:04 +0300

    Update Sayısal Loto.cs

--- Sayısal Loto.cs
+++ Sayısal Loto.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace C_2
 {
@@ -9,7 +9,8 @@ namespace C_2
             int[] sayilar = new int[7];
             int sayi;
             int[] randomSayilar = new int[7];
-
+            Random random = new Random();
+            int bilinen = 0;
             for (int i = 0; i < 7; i++){
                 System.Console.WriteLine("{0}. Sayınızı Girin.",i+1);
                 sayi = Convert.ToInt16(Console.ReadLine());
@@ -19,15 +20,13 @@ namespace C_2
                     System.Console.WriteLine("Lütfen Girdiğiniz Sayıyı Bir Daha Girmeyin.");
                     sayi = Convert.ToInt16(Console.ReadLine());                    
                 }
+                while (sayi<1|| sayi>47) {
+                    System.Console.WriteLine("1'den Küçük 47'den Büyük Sayı Girmeyin.");
+                    sayi = Convert.ToInt16(Console.ReadLine());            
+                }
                 sayilar[i] = sayi;
-            }
-            System.Console.WriteLine("Sayılar Çekiliyor..");
-            Random random = new Random();
-            int bilinen = 0;
-            for (int i = 0; i < 7; i++)
-            {
                 randomSayilar[i] = random.Next(1,47);
-                if (Array.IndexOf(randomSayilar,sayilar) > -1) {
+                if (Array.IndexOf(randomSayilar,sayi) > -1) {
                     System.Console.WriteLine("{0}. Sayınız Aynı Çıktı Tebrikler!",i+1);
                     bilinen = bilinen+1;
                 }else {
